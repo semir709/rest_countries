74be9df216752bd2b3d1a81cbdf12cf5dc211523
semir709 <selmansemir555@gmail.com>
2023-04-25 09:53:25 +0200

    basics style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      "custom-dark-blue": "hsl(209, 23%, 22%)",
+      "custom--very-dark-blue-bg": "hsl(207, 26%, 17%)",
+      "custom-very-dark-blue-text": "hsl(200, 15%, 8%)",
+      "custom-dark-gray": "hsl(0, 0%, 52%)",
+      "custom-light-gray": "hsl(0, 0%, 98%)",
+      "custom-white": "hsl(0, 0%, 100%)",
+    },
+    fontSize: {
+      "custom-home": "14px",
+      "custom-detail": "16px",
+    },
     extend: {},
   },
   plugins: [],
