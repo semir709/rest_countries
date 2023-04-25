153a1931129169b89520d9599c7fcde588b350af
semir709 <selmansemir555@gmail.com>
2023-04-25 15:21:56 +0200

    added basics strucitre for the cards

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="bg-red-300 min-[300px]:w-[280px] w-full h-[300px] my-2 mx-1">
+      Card
+    </div>
+  );
+};
+
+export default Card;
--- src/container/Home.js
+++ src/container/Home.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Header from "../components/Header";
+import Card from "../components/Card";
+
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const Home = ({ dark, setDark }) => {
   return (
     <>
       <Header dark={dark} setDark={setDark} />
+
+      <div className="flex flex-wrap min-[776px]:justify-between justify-around w-full sm:px-[100px] ">
+        {data.map(() => (
+          <Card />
+        ))}
+      </div>
     </>
   );
 };
