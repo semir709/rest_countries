5f6dfdf55696caa131fbb283d1383c43a4f7a0e2
semir709 <selmansemir555@gmail.com>
2023-04-26 12:03:17 +0200

    made the filter nav responsive

--- src/components/FilterNav.js
+++ src/components/FilterNav.js
@@ -4,12 +4,12 @@ import Filter from "./Filter";
 
 const FilterNav = ({ dark }) => {
   return (
-    <div className="my-5 flex flex-wrap justify-between  w-full sm:px-[100px] px-4">
-      <div className="w-1/3">
+    <div className="my-5 min-[550px]:flex flex-wrap justify-between items-center   w-full sm:px-[100px] px-4">
+      <div className="lg:w-1/3 sm:w-1/2 ">
         <SearchBar dark={dark} />
       </div>
 
-      <div className="w-[200px]">
+      <div className="w-[200px] min-[550px]:my-0 my-[50px]">
         <Filter dark={dark} />
       </div>
     </div>
