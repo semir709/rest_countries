6b87904c41f997922a8475819b7b7ce8f86e0548
semir709 <selmansemir555@gmail.com>
2023-04-26 09:53:14 +0200

    added the skeleton loading

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,17 +1,33 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
 import Card from "../components/Card";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const Home = ({ dark, setDark }) => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   return (
     <>
       <Header dark={dark} setDark={setDark} />
 
       <div className="flex flex-wrap min-[776px]:justify-between justify-around w-full sm:px-[100px] ">
-        {loading ? "Loading..." : data.map(() => <Card dark={dark} />)}
+        {loading
+          ? data.map(() => (
+              <div className="min-[300px]:w-[280px] w-full h-[300px] my-2 mx-1 shadow-md rounded-md overflow-hidden cursor-pointer">
+                {dark ? (
+                  <Skeleton
+                    height={"100%"}
+                    baseColor="hsl(209, 23%, 22%)"
+                    highlightColor={"hsl(207, 26%, 17%)"}
+                  />
+                ) : (
+                  <Skeleton height={"100%"} />
+                )}
+              </div>
+            ))
+          : data.map(() => <Card dark={dark} />)}
       </div>
     </>
   );
