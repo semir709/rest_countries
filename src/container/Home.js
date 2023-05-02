4d7ae6d6fe1b7a653afc2df58e283e41c5edbce9
semir709 <selmansemir555@gmail.com>
2023-05-02 20:25:32 +0200

    fixed problem with skeleton loading in the main page

--- src/container/Home.js
+++ src/container/Home.js
@@ -6,7 +6,6 @@ import "react-loading-skeleton/dist/skeleton.css";
 import FilterNav from "../components/FilterNav";
 import axios from "axios";
 
-const el = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const baseUrl =
   "https://restcountries.com/v3.1/all?fields=name,capital,population,region,flags";
 
@@ -23,30 +22,34 @@ const Home = ({ dark, setDark }) => {
   }, []);
 
   useEffect(() => {
-    setData(storeData.slice(index, index + 20));
+    // setData(storeData.slice(index, index + 20));
     setLoading(false);
   }, [storeData, index]);
+
+  const leng = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   return (
     <>
       <Header dark={dark} setDark={setDark} />
       <FilterNav dark={dark} />
 
-      <div className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 col-span-full gap-8 w-full sm:px-[100px] px-[50px] ">
-        {loading && storeData.length === 0
-          ? data.map(() => (
-              <div className="min-[300px]:w-[280px] w-full h-[300px] my-2 mx-1 shadow-md rounded-md overflow-hidden cursor-pointer">
-                {dark ? (
-                  <Skeleton
-                    height={"100%"}
-                    baseColor="hsl(209, 23%, 22%)"
-                    highlightColor={"hsl(207, 26%, 17%)"}
-                  />
-                ) : (
-                  <Skeleton height={"100%"} />
-                )}
-              </div>
-            ))
+      <div className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 col-span-full gap-8 w-full sm:px-[100px] px-[50px] my-5">
+        {data.length === 0
+          ? Array(12)
+              .fill()
+              .map(() => (
+                <div className="min-[300px]:w-[280px] w-full h-[300px] my-2 mx-1 shadow-md rounded-md overflow-hidden cursor-pointer">
+                  {dark ? (
+                    <Skeleton
+                      height={"100%"}
+                      baseColor="hsl(209, 23%, 22%)"
+                      highlightColor={"hsl(207, 26%, 17%)"}
+                    />
+                  ) : (
+                    <Skeleton height={"100%"} />
+                  )}
+                </div>
+              ))
           : data.map((data, index) => (
               <Card key={index} data={data} dark={dark} />
             ))}
